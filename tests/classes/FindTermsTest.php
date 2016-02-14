8aa8b114420942c35b686ac7e8649e5bb29397bf
Guillem Lluch Moll <guillem72@gmail.com>
2016-02-14 23:22:12 +0100

    A test for FindeTerms provided.

--- tests/classes/FindTermsTest.php
+++ tests/classes/FindTermsTest.php
@@ -1,14 +1,19 @@
 <?php
 namespace glluch\phaseExpansion;
-require_once __DIR__."/classes/FindTerms.php";
-
-$taxo0=  unserialize(\file_get_contents(__DIR__."/resources/tests/phpTaxonomy.ser"));
-$expansion =new FindTerms();
-$taxo=$expansion->setTermsFromTaxo($taxo0);
-\file_put_contents("taxo0.ser", \serialize($taxo));
-\var_dump($taxo);
-$text_doc="123 Logic devices and Engineering management and Computational and artificial intelligence  and B-ISDN and Logic devices and B-ISDN";
-$text_doc0="B.2. Component Integration. 
+require_once __DIR__.'/../../classes/FindTerms.php';
+/**
+ * Test FindeTerms class
+ *
+ * @author Guillem LLuch Moll <guillem72@gmail.com>
+ */
+class FindTermsTest extends \PHPUnit_Framework_TestCase {
+   protected $doc;
+   protected $termsFile;
+   protected $solutionFile;
+   protected $taxoFile;
+   
+   function __construct() {
+       $this->doc = "B.2. Component Integration. 
     Integrates hardware, software or sub system components into an existing or a new system. Complies with established processes and procedures such as, configuration management and package maintenance. Takes into account the compatibility of existing and new modules to ensure system integrity, system interoperability and information security. Verifies and tests system capacity and performance and documentation of successful integration.
 Proficiency Levels
 Proficiency Level 2 - Acts systematically to identify compatibility of software and hardware specifications. Documents all activities during installation and records deviations and remedial activities.
@@ -27,9 +32,23 @@ S2 document and record activities, problems and related repair activities
 S3 match customers' needs with existing products
 S4 verify that integrated systems capabilities and efficiency match specifications
 S5 secure/ back-up data to ensure integrity during system integration";
+       $this->termsFile =__DIR__."/../../resources/tests/phpTaxonomy.ser";
+       $this->solutionFile = __DIR__."/../../resources/tests/sol0.ser";
+        $this->taxoFile = __DIR__."/../../resources/tests/taxo0.ser";
+   }
 
-$expansion->setDoc($text_doc0);
-$positions=$expansion->find();
-//var_dump($positions);
-//$expansion->deleteFP();
-//\file_put_contents("sol0.ser", \serialize($positions));
+   public function testDoc0(){
+       $terms=\unserialize(\file_get_contents($this->termsFile));
+       $sol=\unserialize(\file_get_contents($this->solutionFile));
+       $solTaxo=\unserialize(\file_get_contents($this->taxoFile));
+       $expansion =new FindTerms();
+       $taxo=$expansion->setTermsFromTaxo($terms);
+       $expansion->setDoc($this->doc);
+       $positions=$expansion->find();
+       
+       $this->assertEquals($positions,$sol);
+       $this->assertEquals($taxo,$solTaxo);
+   }
+   
+   
+}
